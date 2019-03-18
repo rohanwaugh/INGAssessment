4cfabba154c6b98c3eb0bd2747896b90de89435d
Rohan Wagh <rohan.wagh@cognizant.com>
2019-03-18 16:41:03 +0530

    Code cleaning

--- app/src/main/java/com/cts/sample/model/DataModel.kt
+++ app/src/main/java/com/cts/sample/model/DataModel.kt
@@ -4,12 +4,13 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class DataModel(val name: String,
-                     val realname :String,
-                     val team : String,
-                     val firstappearance : String,
-                     val createdby : String,
-                     val publisher :String,
-                     val imageurl: String) : Parcelable
+data class DataModel(
+    var name: String,
+    var realname :String,
+    val team : String,
+    val firstappearance : String,
+    var createdby : String,
+    val publisher :String,
+    val imageurl: String) : Parcelable
 
 
