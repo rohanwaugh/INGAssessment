9ee45e6d6e66104e153fdd29d222eb5272dc7779
ChandraPrakash gandhwani <CHANDRAPRAKASH.GANDHWANI@cognizant.com>
2019-03-29 14:05:45 +0530

    New Constant class

---
+++ app/src/main/java/com/cts/sample/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.cts.sample.util
+
+class Constants {
+
+    companion object {
+        const val SUCCESS_MSG = "Success"
+        const val ERROR_MSG = "No Data Available"
+        const val BASE_URL = "https://simplifiedcoding.net/demos/"
+
+    }
+}
