4fed42eddfed6140fce5dc3f4d5054e017a40093
ChandraPrakash gandhwani <CHANDRAPRAKASH.GANDHWANI@cognizant.com>
2019-03-29 14:00:05 +0530

    Code Refactoring for DataRepository class

--- app/src/main/java/com/cts/sample/network/DataRepository.kt
+++ app/src/main/java/com/cts/sample/network/DataRepository.kt
@@ -1,19 +1,20 @@
 package com.cts.sample.network
 
 import com.cts.sample.model.DataModel
+import com.cts.sample.util.Constants
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 /* This is Repository class designed to fetch data from network. */
-class DataRepository(val webService: API) {
+class DataRepository(private val heroApi: HeroApi) {
 
     fun getHeros(success: ((List<DataModel>?) -> Unit)?, failure: ((String) -> Unit)?){
 
-        val heroApi =  webService.getHeros()
+        val heroApi =  heroApi.getHeros()
         heroApi.enqueue(object : Callback<List<DataModel>> {
             override fun onFailure(call: Call<List<DataModel>>, t: Throwable?) {
-                failure?.invoke("No Data")
+                failure?.invoke(Constants.ERROR_MSG)
             }
 
             override fun onResponse(call: Call<List<DataModel>>, response: Response<List<DataModel>>) {
