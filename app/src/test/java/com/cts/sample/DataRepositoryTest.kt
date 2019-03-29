a11eecefb8c87462c31718d54f247e499e9858de
ChandraPrakash gandhwani <CHANDRAPRAKASH.GANDHWANI@cognizant.com>
2019-03-29 14:06:48 +0530

    Unit test class for DataRepository

--- app/src/test/java/com/cts/sample/DataRepositoryTest.kt
+++ app/src/test/java/com/cts/sample/DataRepositoryTest.kt
@@ -1,17 +1,24 @@
 package com.cts.sample
 
 import com.cts.sample.model.DataModel
-import com.cts.sample.network.API
+import com.cts.sample.network.HeroApi
 import com.cts.sample.network.DataRepository
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DataRepositoryTest {
 
     @Mock
-    lateinit var webService: API
+    lateinit var heroApi: HeroApi
 
     @Mock
     lateinit var heroList: Call<List<DataModel>>
@@ -21,10 +28,35 @@ class DataRepositoryTest {
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
-        dataRepository = DataRepository(webService)
+        dataRepository = DataRepository(heroApi)
     }
 
-    fun testGetHerosSuccess(){
-        dataRepository.getHeros(success = {},failure = {})
+    @Test
+    fun testGetHerosSuccess() {
+
+        whenever(heroApi.getHeros())
+            .thenReturn(heroList)
+
+        whenever(heroList
+            .enqueue(any()))
+            .thenAnswer {
+                (it.arguments[0] as? Callback<List<DataModel>>)
+                    ?.onResponse(heroList, Response.success(getFakePreviewList()))
+            }
+
+        dataRepository.getHeros({
+            assertTrue(it?.size==2)
+            assertEquals(it?.get(0)?.name,"Captain America")
+        },{})
     }
+
+
+
+    fun getFakePreviewList():List<DataModel>{
+        return listOf(DataModel("Captain America","Steve Rogers", "Avengers", "1941",
+            "Joe Simon", "DummyURL"),DataModel("Wolvorine","James Howlett", "X-Men", "1974",
+            "Len Wein", "DummyURL1"))
+    }
+
+
 }
